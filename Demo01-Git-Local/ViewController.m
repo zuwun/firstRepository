600a0a37af0f56e0c671e86f2aa53527be2f299c
tarena256 <tarena256@tarena256deiMac.local>
2015-07-01 10:27:18 +0800

    add second

--- Demo01-Git-Local/ViewController.m
+++ Demo01-Git-Local/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"this is first commit");
+    
+    NSLog(@"this is second commit");
+
 }
 
 - (void)didReceiveMemoryWarning {
