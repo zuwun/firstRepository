ffd2aec4d55331108ff1f7ed3338a30d45fa5d10
tarena256 <tarena256@tarena256deiMac.local>
2015-07-01 10:24:11 +0800

    add new Test Class

---
+++ Demo01-Git-Local/TestGit.h
@@ -0,0 +1,13 @@
+//
+//  TestGit.h
+//  Demo01-Git-Local
+//
+//  Created by tarena256 on 15/7/1.
+//  Copyright (c) 2015年 tarena256. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestGit : NSObject
+
+@end
---
+++ Demo01-Git-Local/TestGit.m
@@ -0,0 +1,17 @@
+//
+//  TestGit.m
+//  Demo01-Git-Local
+//
+//  Created by tarena256 on 15/7/1.
+//  Copyright (c) 2015年 tarena256. All rights reserved.
+//
+
+#import "TestGit.h"
+
+@implementation TestGit
+
+-(void)testFunction{
+    NSLog(@"....");
+}
+
+@end
